1d49e1ec7c791e76828babd20f3a97de3a930810
Danix <plugrush1990@gmail.com>
2026-07-12 19:59:38 +0300

    design new v2.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
   <head>
     <link rel="icon" href="/icon.svg" />
     <link rel="apple-touch-icon" href="/icon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
   </head>
   <body className="min-h-full flex flex-col">
     <Providers>
